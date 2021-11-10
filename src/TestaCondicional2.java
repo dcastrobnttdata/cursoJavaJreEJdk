8511d365b996f35ef5696b240c904b00ebae6ea0
Diego Barcellos <dcastrob@nttdata.com>
2021-11-09 22:46:21 -0300

    Subindo o projeto do curso de Java JRE e JDK para o GitHub

---
+++ src/TestaCondicional2.java
@@ -0,0 +1,24 @@
+
+public class TestaCondicional2 {
+
+	public static void main(String[] args) {
+		System.out.println("testando condicionais");
+		int idade = 20;
+		int quantidadePessoas = 3;
+		boolean acompanhado = quantidadePessoas >= 2;
+		
+		System.out.println("valor de acompanhado = " + acompanhado);
+
+		if (idade >= 18 && acompanhado) {
+			System.out.println("seja bem vindo");
+		} else {
+			System.out.println("infelizmente você não pode entrar");
+		}
+		
+		int idadeMaior = 68;
+		boolean ehIdoso = idadeMaior >= 65;
+		System.out.println(ehIdoso);
+
+	}
+
+}
